a068bd832787e755bac647e878d5e05f6362fca0
rgrannell1 <r.grannell2@gmail.com>
2013-11-16 15:15:14 +0000

    generate arrow method snippets

--- generate-snippets.sh
+++ generate-snippets.sh
@@ -3,22 +3,27 @@
 method_pattern='x[A-Z]\{1\}[^)]*'
 arrow_namespace_path=/home/ryan/Code/Arrow/arrow-clean/NAMESPACE
 
-function makeSnippet () {
+function makeMethodSnippet () {
+	# create a snippet for a method.
 
 	local method_name=$1
-	local snippet_path=/home/ryan/Code/Arrow/arrow-snippets/$method_name.sublime-snippet
 
-	"
+	local path=/home/ryan/Code/Arrow/arrow-snippets/$method_name.sublime-snippet
+
+	cat <<-EOL > $path
+
 	<snippet>
-		<content><![CDATA[$method_name()]]></content>
-	 	<tabtrigger>x</tabtrigger>
-		<scope>text.R</scope>
-		<description>Method-$method_name</description>
-	</snippet>" > $snippet_path
+	<content><![CDATA[$method_name()]]></content>
+	<tabtrigger>x</tabtrigger>
+	<scope>text.R</scope>
+	<description>Method-$method_name</description>
+	</snippet>
+
+	EOL
 }
 
 grep -o $method_pattern $arrow_namespace_path |
-while read line
+while read method_name
 do
-	makeSnippet $line
+	makeMethodSnippet $method_name
 done
