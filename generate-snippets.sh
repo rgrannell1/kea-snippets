89670314f2603ede30457286acf84f314a11fc8a
rgrannell1 <r.grannell2@gmail.com>
2013-11-16 23:51:47 +0000

    updated and tested snippets

--- generate-snippets.sh
+++ generate-snippets.sh
@@ -7,16 +7,14 @@ function makeMethodSnippet () {
 	# create a snippet for a method.
 
 	local method_name=$1
-
 	local path=/home/ryan/Code/Arrow/arrow-snippets/$method_name.sublime-snippet
 
 	cat <<-EOL > $path
 
 	<snippet>
-	<content><![CDATA[$method_name()]]></content>
-	<tabtrigger>x</tabtrigger>
-	<scope>text.R</scope>
-	<description>Method-$method_name</description>
+	    <description>Arrow-$method_name</description>
+	    <content><![CDATA[$method_name(\${1})]]></content>
+	    <tabTrigger>$method_name</tabTrigger>
 	</snippet>
 
 	EOL
