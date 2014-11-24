c39ac147e098c59a2a5d959738ecd74d421d936b
rgrannell1 <r.grannell2@gmail.com>
2014-11-24 16:56:09 +0000

    updated script to include parametres in snippets

---
+++ generate-snippets.R
@@ -0,0 +1,45 @@
+#!/usr/bin/env Rscript
+
+require(kea)
+
+
+
+make_snippet <- (method_name : description) := {
+	"
+	<snippet>
+	<description>%s</description>
+	<content><![CDATA[%s]]></content>
+	<scope>source.r</scope>
+	<tabTrigger>%s</tabTrigger>
+	</snippet>
+	" -> template
+
+	sprintf(template, description, method_name, method_name)
+
+}
+
+
+
+
+
+
+x_(ls('package:kea'))                             $
+xSelect(
+	xIsMatch('^x[A-Z].*?'))                       $
+xRelate(fn_name := {
+
+	args <- x_( getNamespace('kea') [[fn_name]] ) $ xParamsOf() $ x_Implode(', ')
+
+	xFromChars_(fn_name, '(', args, ')')
+
+})                                                $
+xMap(
+	xJuxtapose_(
+		xFirstOf,
+		xApply(make_snippet)) )                   $
+xMap(
+	xJuxtapose_(
+		xFirstOf %then% (str := xFromChars_(str, '.sublime-snippet')),
+		xSecondOf))                               $
+xDo(
+	xApply(xWrite))
